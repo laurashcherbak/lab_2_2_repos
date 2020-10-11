ac8dc1062973698a5b3f3a2ac01ba1a6796d9093
Laura Shcherbak <laura.shcherbak@gmail.com>
2020-10-11 20:51:32 +0300

    Lab 2.1

--- Lab_2_1.cpp
+++ Lab_2_1.cpp
@@ -9,17 +9,17 @@ using namespace std;
 int main()
 {
 	double a; // вхідний параметр
-	//double z1; // результат обчислення 1-го виразу
+	double z1; // результат обчислення 1-го виразу
 	double z2; // результат обчислення 2-го виразу
 
 	cout << "a = "; 
 	cin >> a;
 
-	//z1 = 1 - ((1 / 4) * sin(2 * a) * sin(2 * a)) + cos(2 * a);
+	z1 = 1 - ((1 / 4) * sin(2 * a) * sin(2 * a)) + cos(2 * a);
 	z2 = (cos(a) * cos(a)) * (1 + (cos(a) * cos(a)));
 
 	cout << endl;
-	//cout << "z1 = " << z1 << endl;
+	cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
 	cin.get();
 
