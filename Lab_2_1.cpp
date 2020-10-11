8d02f2864dc86934524c17eda7b48aa43ce7e9bd
Laura Shcherbak <laura.shcherbak@gmail.com>
2020-10-11 19:06:52 +0300

    lab 2.2: first master commit

---
+++ Lab_2_1.cpp
@@ -0,0 +1,27 @@
+﻿// Lab_2_1.cpp
+// Shcherbak Laura
+// Лабораторна робота No 2.1
+// Лінійні програми.
+// Варіант 25
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double a; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	//double z2; // результат обчислення 2-го виразу
+
+	cout << "a = "; 
+	cin >> a;
+
+	z1 = 1 - ((1 / 4) * sin(2 * a) * sin(2 * a)) + cos(2 * a);
+	//z2 = (cos(a) * cos(a)) * (1 + (cos(a) * cos(a)));
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	//cout << "z2 = " << z2 << endl;
+	cin.get();
+
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+echo "# lab_2_2_repos" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/laurashcherbak/lab_2_2_repos.git
+git push -u origin main
